b08dfecdd5a262834330b67f50b8a1d59e029aa6
Christian Larwood <chrislarwood@gmail.com>
2025-02-19 16:29:54 -0500

    first pass promotions controller actions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,7 +1,21 @@
 class PromotionsController < ApplicationController
   def new
+    @promotion = Promotion.new
   end
 
   def create
+    @promotion = Promotion.new(promotion_params)
+
+    if @promotion.save
+      render @promotion
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :promotable_type, :discount_percentage, :discount_cents, :x_quantity, :y_quantity, :promotable_type, :start_date, :end_date, :weight_threshold)
   end
 end
