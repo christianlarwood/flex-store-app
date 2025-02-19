9e40c0aa1e21d114dd5ce6dfa7a2708c358a8585
Christian Larwood <chrislarwood@gmail.com>
2025-02-19 16:24:45 -0500

    first pass items controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,43 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+
+    if @item.update
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price_cents, :unit_type, :unit_quantity, :unit_of_measurement, :brand_id, :category_id)
   end
 end
