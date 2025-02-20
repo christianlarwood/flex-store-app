7f454049904e66eb74a9e047ed3210b4632ae589
Christian Larwood <chrislarwood@gmail.com>
2025-02-20 10:01:32 -0500

    view all items, view create new item

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :unit_type, presence: true, inclusion: { in: %w(weight, quantity) }
-  validates :unit_quantity, presence: true,
+  validates :unit_quantity, presence: true
   validates :unit_of_measurement, presence: true, inclusion: { in: %w(g, kg, oz, lb, each, pack) }
 end
