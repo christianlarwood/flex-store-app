2a320a4eae82f51f8450a1e628a991323d29df6b
Christian Larwood <chrislarwood@gmail.com>
2025-02-21 14:49:42 -0500

    handle additional items to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,20 @@
 class CartItemsController < ApplicationController
   def create
-    @cart_item = @cart.cart_items.new(cart_item_params)
+    existing_cart_item = @cart.cart_items.find_by(item_id: cart_item_params[:item_id])
 
-    @cart_item.quantity = params[:quantity].to_i
-    @cart_item.price_cents = @cart_item.item.price_cents * @cart_item.quantity
+    if existing_cart_item
+      existing_cart_item.quantity += cart_item_params[:quantity].to_i
+      existing_cart_item.price_cents = existing_cart_item.item.price_cents * existing_cart_item.quantity
+      existing_cart_item.save
+    else
+      @cart_item = @cart.cart_items.new(cart_item_params)
 
-    if @cart_item.save
+      @cart_item.quantity = params[:quantity].to_i
+      @cart_item.price_cents = @cart_item.item.price_cents * @cart_item.quantity
+      @cart_item.save
+    end
+
+    if @cart_item&.save || existing_cart_item&.save
       redirect_to root_path
     else
       redirect_to root_path, status: :unprocessable_entity
